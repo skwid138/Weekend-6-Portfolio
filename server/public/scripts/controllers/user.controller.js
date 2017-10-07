5ca5507ca8717c7cd7b95a5c0ce63bd008ec66c6
Hunter Rancourt <skwid138@gmail.com>
2017-10-06 20:18:50 -0500

    add more notes

--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -2,11 +2,15 @@ myApp.controller('UserController', function (GithubService) {
   console.log('UserController loaded.')
   var vm = this;
 
+  // github user data
   vm.user = GithubService.user;
+
+  // github repo data
   vm.repos = GithubService.repos;
 
+
+  
   // get profile and repo info on load
   GithubService.githubProfile();
   GithubService.githubRepos();
-
-});
+}); // end controller
--- server/public/scripts/services/github.service.js
+++ server/public/scripts/services/github.service.js
@@ -1,8 +1,11 @@
 myApp.service('GithubService', function ($http) {
   var self = this;
-  // Object that will store our user data
 
+  // Object that will store our user data
   self.user = {data: {}};
+
+  // Object that will store our repo data
+  self.repos = { data: [] };
 
   //Call to Github API to fetch user's profile info
   self.githubProfile = function () {
@@ -12,11 +15,9 @@ myApp.service('GithubService', function ($http) {
     }).then(function (response) {
       console.log(response.data);
       self.user.data = response.data;
-    });
-  }
-
-  // Object that will store our repo data
-  self.repos = { data: [] };
+    }); // end $http
+  }; // end githubProfile
+  
   //Call to Github API to fetch list of user's repos
   self.githubRepos = function () {
     $http({
@@ -25,6 +26,6 @@ myApp.service('GithubService', function ($http) {
     }).then(function (response) {
       console.log(response.data);
       self.repos.data = response.data;
-    });
-  }
-});
+    }); // end $http
+  }; // end githubRepos
+}); // end service
